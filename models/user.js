9d916a8fe660b30d3e03f1a5c32d1179a64af976
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-01 11:27:38 -0500

    google oauth connected

--- models/user.js
+++ models/user.js
@@ -4,12 +4,12 @@ const knex = require('knex')(config.development);
 
 // @TODO is it good to set a Promise?
 
-function findById(googleID) {
-  console.log('googleID:', googleID.googleid);
+function findById(googleID, accessToken, refreshToken) {
+  console.log('googleID:', googleID);
   return new Promise(function (resolve, reject) {
     knex.select()
     .from('task_users')
-    .where('googleid', googleID.googleid)
+    .where('googleid', googleID)
     .then(function (data) {
       resolve(data[0]);
     }).catch(function (err) {
@@ -19,15 +19,16 @@ function findById(googleID) {
   });
 };
 
-function create(googleID) {
+function create(googleID, accessToken, refreshToken) {
   console.log('inside create');
   return new Promise(function (resolve, reject) {
 
-    knex.insert({googleid: googleID.googleid})
+    knex.insert({googleid: googleID, accesstoken: accessToken, refreshtoken: refreshToken})
         .into('task_users')
+        .returning('*')
         .then(function (response) {
           console.log('response', response);
-          resolve(response);
+          resolve(response[0]);
         }).catch(function (err) {
           console.log('Error Querying the DB', err);
           return reject(err);
--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -4,7 +4,8 @@ const passport = require('passport');
 router.get('/',
   passport.authenticate('google', { scope:
     ['https://www.googleapis.com/auth/plus.login',
-    'https://www.googleapis.com/auth/plus.profile.emails.read']}
+    'https://www.googleapis.com/auth/plus.profile.emails.read',
+    'https://www.googleapis.com/auth/calendar']}
   ));
 
 router.get('/callback',
