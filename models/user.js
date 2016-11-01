84fe6ca9b83b2a67cd210561b11cc4fc4ca940b2
Alex Whitaker <whitaker.alexander@gmail.com>
2016-10-31 21:50:22 -0500

    login successful if id is already in db

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const config = require('../db/connections.js');
 const knex = require('knex')(config.development);
 
+// @TODO is it good to set a Promise?
+
 function findById(googleID) {
   console.log('googleID:', googleID.googleid);
   return new Promise(function (resolve, reject) {
@@ -9,8 +11,7 @@ function findById(googleID) {
     .from('task_users')
     .where('googleid', googleID.googleid)
     .then(function (data) {
-      resolve(null);
-      // @TODO: this is where I'm stuck it gets to here and fails
+      resolve(data[0]);
     }).catch(function (err) {
       console.log('Error Querying the DB', err);
       return reject(err);
@@ -19,14 +20,14 @@ function findById(googleID) {
 };
 
 function create(googleID) {
+  console.log('inside create');
   return new Promise(function (resolve, reject) {
 
-    knex.insert(googleID.googleid)
+    knex.insert({googleid: googleID.googleid})
         .into('task_users')
-        .returning()
         .then(function (response) {
           console.log('response', response);
-          res.sendStatus(200);
+          resolve(response);
         }).catch(function (err) {
           console.log('Error Querying the DB', err);
           return reject(err);
