67f98b140ada4ac70f9ac6ca1f8e800b88372fbc
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-03 11:01:09 -0500

    refactor with promise

--- routes/googleCalendar.js
+++ routes/googleCalendar.js
@@ -7,6 +7,8 @@ var OAuth2 = google.auth.OAuth2;
 
 const auth = require('../db/auth');
 
+var calendar = google.calendar('v3');
+
 var oauth2Client = new OAuth2(
   auth.googleAuth.clientID,
   auth.googleAuth.clientSecret,
@@ -22,38 +24,47 @@ function getCalendarEvents(req, res) {
     refresh_token: req.user.refreshtoken,
   });
   console.log('oauth2Client', oauth2Client);
-  var OMGEVENTS = listEvents(oauth2Client);
-  res.send(OMGEVENTS);
+  listEvents(oauth2Client).then(function (futureEvents) {
+    res.send(futureEvents);
+  }).catch(function (err) {
+    console.log('err', err);
+    res.sendStatus(500);
+  });
 };
 
 function listEvents(auth) {
-  var calendar = google.calendar('v3');
-  calendar.events.list({
-    auth: auth,
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, function (err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
+  return new Promise(function (resolve, reject) {
 
-    var events = response.items;
-
-    return events;
-    if (events.length == 0) {
-      console.log('No upcoming events found.');
-    } else {
-      console.log('Upcoming 10 events:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        console.log('%s - %s', start, event.summary);
+    calendar.events.list({
+      auth: auth,
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, function (err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return reject(err);
       }
-    }
+
+      var events = response.items;
+      // console.log('response.items', events);
+
+      return resolve(events);
+
+      if (events.length == 0) {
+        console.log('No upcoming events found.');
+      } else {
+        console.log('Upcoming 10 events:');
+        for (var i = 0; i < events.length; i++) {
+          var event = events[i];
+          var start = event.start.dateTime || event.start.date;
+          console.log('%s - %s', start, event.summary);
+        }
+      }
+    });
+
   });
 }
 
