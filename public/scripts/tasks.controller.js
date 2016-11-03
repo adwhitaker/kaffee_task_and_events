ffbf20edd9cc6a427d9b40713bcf6279b37f1378
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-03 14:13:01 -0500

    get daily tasks

--- public/scripts/tasks.controller.js
+++ public/scripts/tasks.controller.js
@@ -1,6 +1,21 @@
 angular.module('tasksApp')
        .controller('TasksController', TasksController);
 
-function TasksController() {
+function TasksController($http) {
   console.log('TasksController loaded');
+  var tasks = this;
+
+  tasks.getEverydayTasks = function () {
+    $http.get('/dailytasks').then(printEverydayTasks, errorCallback);
+  };
+
+  tasks.getEverydayTasks();
+};
+
+function errorCallback(error) {
+  console.log('error making http request', error);
+};
+
+function printEverydayTasks(response) {
+  console.log('daily tasks', response);
 };
