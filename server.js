eda2d97b26762e8319ff9b3dc507c1e382ee082f
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-11 12:50:40 -0600

    add comments/remove test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const dailyTasks = require('./routes/dailyTasks');
 var app = express();
 
 const sessionConfig = {
-  secret: 'super secret key goes here', // TODO this should be read from ENV
+  secret: 'super secret',
   key: 'user',
   resave: true,
   saveUninitialized: true,
@@ -23,6 +23,7 @@ const sessionConfig = {
   },
 };
 
+// middleware
 auth.setup();
 app.use(session(sessionConfig));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,24 +32,22 @@ app.use(express.static('public'));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// route connections
 app.use('/auth/google', googleAuth);
 app.use('/calendar', googleCalendar);
 app.use('/tasks', tasks);
 app.use('/dailytasks', dailyTasks);
 
+// is authenticated
 app.get('/*', function (req, res) {
   if (req.isAuthenticated()) {
     res.sendFile(path.join(__dirname, 'public/views/index.html'));
   } else {
     res.redirect('/auth/google');
   }
-
 });
 
-app.get('/test', function (req, res) {
-  res.send(req.user);
-});
-
+// server connection
 var server = app.listen(3000, function () {
   console.log('Listening on port', server.address().port);
 });
