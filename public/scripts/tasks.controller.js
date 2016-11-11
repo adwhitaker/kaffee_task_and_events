adb4d655efd768a543d53f2308e434925aab3cec
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-11 11:28:14 -0600

    add comments

--- public/scripts/tasks.controller.js
+++ public/scripts/tasks.controller.js
@@ -2,15 +2,16 @@ angular.module('tasksApp')
        .controller('TasksController', TasksController);
 
 function TasksController($http, tasksService, eventsService, everyDayTasksService) {
-  console.log('TasksController loaded');
   var tasks = this;
 
+  // everyday tasks displayed index.html
   tasks.boxTasks = everyDayTasksService;
 
   tasks.logout = function () {
     window.location = '/logout';
   };
 
+  // function to update completed daily tasks once click in index.html
   tasks.completeDaily = function (id, item1, amount, item2, complete) {
     var completeOpposite = !complete;
 
@@ -20,6 +21,7 @@ function TasksController($http, tasksService, eventsService, everyDayTasksServic
                         });
   };
 
+  // initial http requests to get tasks and events from DB and Google Calendar
   tasksService.getTasks();
   eventsService.getCalendarEvents();
   everyDayTasksService.getEveryDayTasks();
