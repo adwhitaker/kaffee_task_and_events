4dbf02ece38ec57aea4afd465d8d23bc6a25f3b7
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-19 19:19:30 -0600

    refactor to express route

--- routes/logout.js
+++ routes/logout.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const config = require('../db/connections.js');
 
-router.get('/', function (req, res) {
-  console.log('inside logout');
+router.route('/')
+      .get(userLogout);
+
+function userLogout(req, res) {
   req.logout();
   res.redirect('/');
-});
+};
 
 module.exports = router;
