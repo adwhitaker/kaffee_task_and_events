d65bc20d1fd7412b62363474f7e3c2661118bb23
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-21 13:25:22 -0600

    update for heroku

--- db/connections.js
+++ db/connections.js
@@ -1,9 +1,19 @@
+const url = require('url')
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
 // DB setup for Knex.js
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: 'rho'
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true
     }
   }
 };
